cf576d144a76f857a6fd6f040450569e726743a0
Jack Hay <jjphay@gmail.com>
2022-01-14 17:20:44 +0000

    Update dev environment

--- api/go.mod
+++ api/go.mod
@@ -2,11 +2,11 @@ module reiform.com/mynah
 
 go 1.17
 
-require github.com/DataDog/go-python3 v0.0.0-20191126174558-6ed25e33b3c4
-
 require (
+	github.com/go-python/cpy3 v0.0.0-20211127141159-9ab339a58efd // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/graphql-go/graphql v0.8.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.10 // indirect
 )
---
+++ api/model/file.go
@@ -0,0 +1,25 @@
+package model
+
+//the permissions a user can have for a project
+type FileLocation string
+
+const (
+	Local FileLocation = "local"
+	S3    FileLocation = "s3 "
+)
+
+//Defines a file managed by Mynah
+type MynahFile struct {
+	//the id of the file
+	Uuid string `json:"uuid"`
+	//the organization this file belongs to
+	OrgId string `json:"-"`
+	//the owner of the file by uuid (allowed to add and remove the file from projects)
+	OwnerUuid string `json:"owner_uuid"`
+	//the name of the file
+	Name string `json:"name"`
+	//the location of the file
+	Location FileLocation `json:"-"`
+	//the path to the file
+	Path string `json:"-"`
+}

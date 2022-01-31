7515f5956c63010b893402d7ac9f056f0654eb30
Jake Lanning <lanning.jake.w@gmail.com>
2022-01-31 23:13:12 +0000

    Resolve "Remove MaterialUI, add tailwind/headless"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,31 @@
-import './App.css';
+import { Switch } from "@headlessui/react";
+import clsx from "clsx";
+import React from "react";
 
 function App() {
+  const [isOn, setOn] = React.useState(false);
+  const color = isOn ? "blue" : "red";
   return (
-    <div className="App">
-      The humble beginnings of Reiform Mynah
-    </div>
+    <>
+      <h1 className="text-center my-3">
+        The humble beginnings of Reiform Mynah
+      </h1>
+      <hr className="border border-black mx-10" />
+      <Switch
+        className="flex gap-3 items-center justify-center mx-auto my-5"
+        checked={isOn}
+        onChange={() => setOn(!isOn)}
+      >
+        <p className="pointer-events-none">
+          Click this text or the tile to change its color
+        </p>
+        <div
+          className={clsx("w-10 h-10", isOn ? "bg-blue-400" : "bg-red-400")}
+        />
+      </Switch>
+      <hr className="border border-black mx-48" />
+      <p className="text-center mt-1">The tile is {color}</p>
+    </>
   );
 }
 

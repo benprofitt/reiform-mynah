432d15b82fe2c7bfd327d4f6a87061146b062bbd
Jake Lanning <lanning.jake.w@gmail.com>
2023-03-27 22:33:20 -0400

    update dataset list to use a react-table

--- client/src/components/dataset_list.tsx
+++ client/src/components/dataset_list.tsx
@@ -1,13 +1,47 @@
 import { useQuery } from "@tanstack/react-query";
 import { MynahDataset, Paginated } from "../types";
 import makeRequest from "../utils/apiFetch";
-import DatasetListItem from "./dataset_list_item";
+import { ReactComponent as PlaceHolderImg } from "../assets/LAXPlaceholder.svg";
+
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useState } from "react";
+import getDate from "../utils/date";
+
+const columnHelper = createColumnHelper<Omit<MynahDataset, "dataset_id">>();
+
+const columns = [
+  columnHelper.accessor("dataset_name", {
+    cell: (info) => info.getValue(),
+    header: () => <span>Name</span>,
+  }),
+  columnHelper.accessor("date_created", {
+    cell: (info) => getDate(info.getValue()),
+    header: () => <span>Created</span>,
+  }),
+  columnHelper.accessor("date_modified", {
+    cell: (info) => getDate(info.getValue()),
+    header: () => <span>Last modified</span>,
+  }),
+  columnHelper.accessor("dataset_type", {
+    cell: (info) => <span className="capitalize">{info.getValue().replace('_', ' ')}</span>,
+    header: () => <span>Dataset Type</span>,
+  }),
+];
 
 export default function DatasetList(): JSX.Element {
   const { data, isLoading, isError } = useQuery(["datasets"], () =>
     makeRequest<Paginated<MynahDataset>>("GET", "/api/v2/dataset/list")
   );
-
+  const table = useReactTable({
+    data: data ? data.contents : [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
   if (data === undefined || isLoading) {
     return <span>Loading...</span>;
   }
@@ -16,10 +50,40 @@ export default function DatasetList(): JSX.Element {
     return <span>Error retrieving datasets</span>;
   }
   return (
-    <ul>
-      {data.contents.map((MynahDataset, ix) => (
-        <DatasetListItem key={ix} dataset={MynahDataset} />
-      ))}
-    </ul>
+    <>
+    <h4>{data.total} total datasets</h4>
+    <table className="w-full border-separate border-spacing-y-[10px]">
+      <thead className="text-left">
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            <th></th>
+            {headerGroup.headers.map((header) => (
+              <th key={header.id}>
+                {header.isPlaceholder
+                  ? null
+                  : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {table.getRowModel().rows.map((row) => (
+          <tr key={row.id} className="hover:shadow-floating cursor-pointer bg-white h-[70px] border border-grey4 rounded-md text-[18px]">
+            <td><PlaceHolderImg /></td>
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id}>
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    </>
+    
   );
 }
